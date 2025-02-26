03ac967aec0a6977cea320e386a1d5a22e3f6747
mabdullahjs <mabdullah2037@outlook.com>
2025-02-26 19:44:49 +0500

    ts started

---
+++ 02-Typescript/app.js
@@ -0,0 +1,44 @@
+// TS Compiler //p
+// Type Annotations //p
+// Type Inference //p
+// Type Aliases
+// Union and Intersection Types
+// Interfaces
+// Type Aliases vs Interface
+// Generics
+// Enums
+// Tuples
+// Type Guards
+// Type Annotations
+// let user:string = "abdullah";
+// user = "usman"
+// let age:number = 20
+// let isLoggedIn:boolean = true
+// let fruits: string[] = ["asdsad" , "asdasd"] 
+// console.log(user);
+// console.log(age);
+// console.log(isLoggedIn);
+// Type Inference
+// let username = "Abdullah";
+// username = 23
+// let fruit = ['apple' , 'banana' , 'grape' , 'mango']
+// console.log(fruit);
+// Type Aliases
+// type user = string
+// let username:user = "abdullah"
+// type User = {
+//     name: string;
+//     age: number;
+//     email: string;
+//     isLoggedIn?: boolean;
+// }
+// const manager = {
+//     user: "mabdullah",
+//     email: 'mabdullah@gmail.com'
+// }
+// manager.age = 19
+// const user: User = {
+//     name: 'abdullah',
+//     age: 21,
+//     email: 'mabdullah@gmail.com',
+// }
