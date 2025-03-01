a7ef0b7e819e551d672c665df6c28e5b6dfbd637
mabdullahjs <mabdullah2037@outlook.com>
2025-03-01 12:56:09 +0500

    typescript

--- 02-Typescript/app.js
+++ 02-Typescript/app.js
@@ -1,14 +1,14 @@
 // TS Compiler //p
 // Type Annotations //p
 // Type Inference //p
-// Type Aliases
-// Union and Intersection Types
-// Interfaces
-// Type Aliases vs Interface
-// Generics
-// Enums
-// Tuples
-// Type Guards
+// Type Aliases //p
+// Union Types //p
+// Interfaces //p
+// Type Aliases vs Interface //p
+// Type Guards //p
+// Generics //p
+// Enums //p
+// Tuples //p
 // Type Annotations
 // let user:string = "abdullah";
 // user = "usman"
@@ -42,3 +42,92 @@
 //     age: 21,
 //     email: 'mabdullah@gmail.com',
 // }
+// let city = {
+//     name: 'karachi',
+//     population: 240000000, 
+// }
+// city.age = 20;
+// type User = {
+//     username: string;
+//     age: number;
+//     isLoggedIn?: boolean,
+//     greetUser: ()=> void
+// }
+// const user: User = {
+//     username: 'mabdullah',
+//     age: 21,
+//     greetUser: function (){
+//         console.log(`hello ${this.username}`);
+//     }
+// }
+// user.isLoggedIn = true
+// user.greetUser();
+//Union Types
+// let user:string | number = 'mabdullah';
+// let arr: (string | number)[] = ['dasd', 20]
+// function greetUser(username:string , age: number): string{
+//     return `hello ${username} with age ${age}`
+// }
+// console.log(greetUser('abdullah' , 20));
+// function hello(arg: string | number){
+//     if(typeof(arg) === 'string'){
+//         return arg.toLowerCase()
+//     }
+//     return arg.toFixed(2)
+// }
+// console.log(hello(343));
+// interface
+// interface User {
+//     username: string;
+//     age: number;
+//     isLoggedIn?: boolean,
+//     greetUser: () => void
+// }
+// const user:User = {
+//     username: "mabdullah",
+//     age: 21,
+//     greetUser: function (){
+//         console.log(`hello ${this.username}`);
+//     }
+// }
+// Type Aliases vs Interface
+// interface Fruit  {
+//     name: string;
+//     state: string;
+// }
+// interface Mango extends Fruit {
+//     price: number
+// }
+// const mango: Mango = {
+//     name: 'mango',
+//     state: 'sindh',
+//     price: 2000,
+// }
+// Generic
+// function identitytwo(arg: any): any {
+//     return String(arg)
+// }
+// console.log(typeof identitytwo(20));
+// function identity<T>(arg: T): T {
+//     return arg
+// }
+// console.log(identity(20));
+// Enum
+// enum CardinalDirections {
+//     North,
+//     East,
+//     South,
+//     West
+// }
+// let currentDirection = CardinalDirections.North;
+// enum StatusCodes {
+//     NotFound = 404,
+//     Success = 200,
+//     Accepted = 202,
+//     BadRequest = 400
+//   }
+// console.log(StatusCodes.NotFound);
+// Tuples
+// type user = [string, number , boolean ]
+// let arr: user = ['sadsads' , 23 , true]
+// arr.push('cde')
