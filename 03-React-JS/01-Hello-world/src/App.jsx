874a10e3bd04f32d6306898056fe183628c9ea21
mabdullahjs <mabdullah2037@outlook.com>
2025-03-02 12:36:32 +0500

    react started

---
+++ 03-React-JS/01-Hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./About"
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae labore porro quas iste repellat eos sint quasi sed non, beatae autem est officiis quisquam voluptatem culpa reprehenderit consequatur error natus.</p>
+      <About />
+    </>
+  )
+}
+
+export default App
+
+
+// React app
+// components
+// React.fragments
---
+++ 03-React-JS/01-Hello-world/src/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return (
+        <div>
+            <h1>Hello contact</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+
+export default Contact
