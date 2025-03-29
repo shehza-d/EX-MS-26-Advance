0a85fac52ba0a590248542ef3db940821dd869d4
mabdullahjs <mabdullah2037@outlook.com>
2025-03-29 12:52:33 +0500

    shadcn + quiz app

--- 03-React-JS/06-Quiz-App/src/App.jsx
+++ 03-React-JS/06-Quiz-App/src/App.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const [questions, setQuestions] = useState(null)
   const [index, setIndex] = useState(0)
   const [error, setError] = useState(false)
+  const [result , setResult] = useState(0);
+
+  const input = useRef([]);
   useEffect(() => {
     fetch('https://the-trivia-api.com/v2/questions')
       .then(res => res.json())
@@ -19,24 +22,51 @@ const App = () => {
       })
   }, [])
 
-  function shuffleArray(arr){
-    return arr
+  function shuffleArray(arr) {
+    const emptyArr = []
+    const shuffleArr = []
+    for (let i = 0; i < arr.length; i++) {
+      const randomNumber = Math.floor(Math.random() * arr.length)
+      if (emptyArr.includes(randomNumber)) {
+        // console.log("number already mujood ha");
+        i--
+      } else {
+        emptyArr.push(randomNumber)
+        // console.log(randomNumber);
+        shuffleArr[randomNumber] = arr[i]
+      }
+
+    }
+    return shuffleArr
+
+  }
+
+  function nextQuestion() {
+    const selectedOption = input.current.find(item => item && item.checked);
+    console.log(selectedOption.value);
+
+    if(questions[index].correctAnswer === selectedOption.value){
+      setResult(result + 10)
+    }
+
     
+    index < questions.length - 1 ? setIndex(index + 1) : null
   }
   return (
     <>
       <h1>Quiz App</h1>
+      <h1>Result {result} / 100</h1>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error occured</h1>}
       {questions && <div>
         <h1>Q{index + 1} {questions[index].question.text}</h1>
-        {[...questions[index].incorrectAnswers , questions[index].correctAnswer].map((item , index) => {
-          return <div key={index}> 
-            <input type="radio" name="choice" id={item} />
+        {shuffleArray([...questions[index].incorrectAnswers, questions[index].correctAnswer]).map((item, index) => {
+          return <div key={index}>
+            <input type="radio" name='question' value={item} id={item} ref={el => input.current[index] = el} />
             <label htmlFor={item}>{item}</label>
           </div>
         })}
-        <button onClick={() => setIndex(index + 1)}>next</button>
+        <button onClick={nextQuestion}>next</button>
       </div>}
     </>
   )
@@ -49,8 +79,8 @@ export default App
 
 // noon.com 
 // material UI products
-// quiz app
 // typescript + advance js quiz
+// quiz app (next week)
 
 // ELIMINATE
 
---
+++ 03-React-JS/07-Shadcn/src/App.tsx
@@ -0,0 +1,39 @@
+import { Button } from "./components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./components/ui/drawer"
+
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>Button</Button>
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <Button>Button</Button>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default App
