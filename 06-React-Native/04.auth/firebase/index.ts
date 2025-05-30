b55356bde549470cd466e6cd1a14ed0bb4f2d244
shehzad iqbal <shehzaddiqbal@gmail.com>
2025-05-30 17:24:13 +0500

    rename

---
+++ 06-React-Native/04.auth/firebase/index.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAemShN-igBNv_4Ybv6DWSolgBozz_pu28",
+  authDomain: "expertizo-1.firebaseapp.com",
+  projectId: "expertizo-1",
+  storageBucket: "expertizo-1.firebasestorage.app",
+  messagingSenderId: "275526068487",
+  appId: "1:275526068487:web:8cdd8c6d28564097853da2",
+};
+
+// front end per env variables secure ho hi nhi sagty
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
